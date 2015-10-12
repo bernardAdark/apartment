8d5c8cd41f194b4f54da53692dcd4895d55a3656
Yaw Boakye <wheresyaw@gmail.com>
2015-10-12 14:22:15 +0000

    Fetch homes, order by the latest to be updated.

--- app/routes/homes.js
+++ app/routes/homes.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('home', {booked: false});
+    return this.store.findAll('home', {orderBy: 'updatedAt'});
   }
 });
