9bf3211970c6def78c51e5923237306125feff0c
Yaw Boakye <wheresyaw@gmail.com>
2015-10-12 14:24:24 +0000

    Use `findRecord` instead of find.

---
+++ tests/unit/routes/town/homes-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:town/homes', 'Unit | Route | town/homes', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
