a8771e981e076b6c5127490f16e2924096bb2fd8
Yaw Boakye <wheresyaw@gmail.com>
2015-10-13 10:24:40 +0000

    Change `lowercased_name` to `slug`.

--- app/routes/town.js
+++ app/routes/town.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.query('town', {
-        orderBy: 'lowercased_name',
+        orderBy: 'slug',
         equalTo: Ember.String.dasherize(params.town_name.toLowerCase())
       }).
       then((towns) => { return towns.objectAt(0); }).
