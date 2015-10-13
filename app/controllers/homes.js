c2a969111760241b22a6135996744dd24f1641d8
Yaw Boakye <wheresyaw@gmail.com>
2015-10-13 14:11:09 +0000

    Add `createHome` action to homes controller.

--- app/controllers/homes.js
+++ app/controllers/homes.js
@@ -2,5 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   queryParams: ['u'],
-  u: null
+  u: null,
+
+  actions: {
+    createHome() {
+      var newHome = this.store.createRecord('home', {
+        description: this.get('description'),
+        price: this.get('price'),
+        period: this.get('period'),
+        rooms: this.get('rooms'),
+        kitchen: this.get('kitchen'),
+        bathroom: this.get('bathroom')
+      });
+
+      newHome.save().catch((error) => {
+        console.log(error.errors);
+      })
+    }
+  }
 });
