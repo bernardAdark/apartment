beeb9942db75a4dc763325eeddf04d9f31077615
Yaw Boakye <wheresyaw@gmail.com>
2015-10-13 14:10:13 +0000

    Bugfix: Pass suburb's slug to `transitionToRoute`

--- app/controllers/suburbs.js
+++ app/controllers/suburbs.js
@@ -4,9 +4,6 @@ export default Ember.Controller.extend({
   town: null,
   actions: {
     createSuburb() {
-      console.log(this.get('name'));
-      console.log(this.get('description'));
-
       var newSuburb = this.store.createRecord('suburb', {
         name: this.get('name'),
         description: this.get('description')
@@ -16,7 +13,7 @@ export default Ember.Controller.extend({
       suburbOf.get('suburbs').addObject(newSuburb);
       newSuburb.save().then(() => { return suburbOf.save(); })
 
-      this.transitionToRoute('suburbs');
+      this.transitionToRoute('town.suburbs', suburbOf.get('slug'));
     }
   }
 });
