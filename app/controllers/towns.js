94418a2219bcaf6c9c71ec4d91b883dedd6b0451
Yaw Boakye <wheresyaw@gmail.com>
2015-10-13 14:11:30 +0000

    Generate town/suburbs slug during creation/update

--- app/controllers/towns.js
+++ app/controllers/towns.js
@@ -5,7 +5,8 @@ export default Ember.Controller.extend({
     createTown() {
       var newTown = this.store.createRecord('town', {
         name: this.get('name'),
-        description: this.get('description')
+        description: this.get('description'),
+        slug: Ember.String.dasherize(this.get('name'))
       });
 
       newTown.save().
@@ -14,20 +15,21 @@ export default Ember.Controller.extend({
           // Stay here. Record wasn't successfully created.
         });
 
-      this.transitionToRoute('town', newTown.get('lowercased_name'));
+      this.transitionToRoute('town', newTown.get('slug'));
     },
 
     updateTown(model) {
       model.setProperties({
         name: this.get('model.name'),
-        description: this.get('model.description')
+        description: this.get('model.description'),
+        slug: Ember.String.dasherize(this.get('model.name'))
       })
 
       model.save().catch((error) => {
         console.log(error.errors);
       })
 
-      this.transitionToRoute('town', model.get('lowercased_name'));
+      this.transitionToRoute('town', model.get('slug'));
     }
   }
 });
