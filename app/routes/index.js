88dc737ab0b5359e320b41245fa7c8b4166c99ff
Yaw Boakye <wheresyaw@gmail.com>
2015-10-17 08:35:29 +0000

    Add price per month to home model.
    The price captured is the price the home is going for, for the specified
    period. Price per month is therefore the result of their division.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
-    // return this.store.findAll('home');
-		var self = this;
 		return Ember.RSVP.hash({
-      towns: self.store.findAll('town'),
-      suburbs: self.store.findAll('suburb'),
-      homes: self.store.findAll('home')
+      towns: this.store.findAll('town'),
+      suburbs: this.store.findAll('suburb'),
+      homes: this.store.findAll('home')
 		});
   }
 });
