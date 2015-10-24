0e1c946758b18e74c91fce4e4459795d50d1ecad
Yaw Boakye <wheresyaw@gmail.com>
2015-10-24 16:03:51 +0000

    Add controllers/hosts/new.js for hosts.new route.
    Previously, I was using One-Controller-to-Rule-'Em-All. That isn't
    working well, and the controller is growing out of control (pun
    unintended).

--- app/controllers/hosts.js
+++ app/controllers/hosts.js
@@ -1,22 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  actions: {
-    createHost() {
-      let newHost = this.store.createRecord('host', {
-        title: this.get('title'),
-        firstName: this.get('firstName'),
-        lastName: this.get('lastName'),
-        maritalStatus: this.get('maritalStatus'),
-        occupation: this.get('occupation'),
-        spouseTitle: this.get('spouseTitle'),
-        spouseFirstName: this.get('spouseFirstName'),
-        spouseLastName: this.get('spouseLastName'),
-        story: this.get('story')
-      });
-
-      newHost.save().catch(error => console.log(error.errors));
-      this.transitionToRoute('homes.new');
-    }
-  }
 });
---
+++ app/controllers/hosts/new.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  photo: '',
+  gender: 'M',
+  phoneNumbers: [],
+
+  actions: {
+    imageLoaded(image) {
+      this.set('photo', image);
+    },
+
+    createHost() {
+      let newHost = this.store.createRecord('host', {
+        title: this.get('title'),
+        firstName: this.get('firstName'),
+        lastName: this.get('lastName'),
+        maritalStatus: this.get('maritalStatus'),
+        occupation: this.get('occupation'),
+        spouseTitle: this.get('spouseTitle'),
+        spouseFirstName: this.get('spouseFirstName'),
+        spouseLastName: this.get('spouseLastName'),
+        photo: this.get('photo'),
+        phoneNumbers: this.get('phoneNumbers'),
+        gender: this.get('gender')
+      });
+
+      newHost.save().
+        then(this.transitionToRoute('homes')).
+        catch(e => console.log(e.errors));
+    }
+  }
+});
