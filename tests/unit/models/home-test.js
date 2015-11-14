bca531b0a57aa26028d0eb8bf2c0fc7514f18d07
Yaw Boakye <wheresyaw@gmail>
2015-11-14 09:45:45 +0000

    Test model validations

--- tests/unit/models/home-test.js
+++ tests/unit/models/home-test.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('home', 'Unit | Model | home', {
-  needs: ['model:host', 'model:suburb']
+  needs: ['model:host', 'model:suburb', 'ember-validations@validator:local/presence']
 });
 
 
@@ -21,3 +21,39 @@ test('suburb relationship', function() {
   equal(relationship.key, 'suburb');
   equal(relationship.kind, 'belongsTo');
 });
+
+test('summary should be required', function() {
+  let model = this.subject();
+  Ember.run(function() {
+    model.set('summary', null);
+  });
+  
+  equal(model.get('isValid'), false, 'Home is valid without a `summary`');
+});
+
+test('description should be required', function() {
+  let model = this.subject();
+  Ember.run(function() {
+    model.set('description', '');
+  });
+
+  equal(model.get('isValid'), false, 'Home is valid without a `description`');
+});
+
+test('should have at least one bedroom', function() {
+  let model = this.subject();
+  Ember.run(function() {
+    model.set('bedrooms', 0);
+  });
+
+  equal(model.get('isValid'), false, 'Home is valid with no bedrooms');
+});
+
+test('should not have negative bedrooms', function() {
+  let model = this.subject();
+  Ember.run(function() {
+    model.set('bedrooms', -9);
+  });
+
+  equal(model.get('isValid'), false, 'Home is valid with negative number of bedrooms');
+})
