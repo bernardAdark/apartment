421d6a4e444e7b0f7075d515c0d8a0c8c0a7e5f5
Yaw Boakye <wheresyaw@gmail.com>
2015-11-16 14:20:30 +0000

    Remove `pluralize-text` helper
    Use `Ember.String.pluralize` or call String's `pluralize` method
    directly.

--- app/helpers/pluralize-text.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export function pluralizeText(params, namedArgs) {
-  var word = params[0];
-  return namedArgs.count === 0 || namedArgs.count > 1 ? word + 's' : word;
-}
-
-export default Ember.Helper.helper(pluralizeText);
