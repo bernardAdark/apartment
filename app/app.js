247b965803726d15e7da5b875bd84e9a6ff1f228
Yaw Boakye <wheresyaw@gmail>
2015-11-20 12:13:14 +0000

    Upgrade
    - Ember@2.2.0
    - Ember-Data@2.2.0
    - Ember-CLI@1.13.13

--- app/app.js
+++ app/app.js
@@ -3,9 +3,11 @@ import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
+let Apartments;
+
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-let Apartments = Ember.Application.extend({
+Apartments = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
