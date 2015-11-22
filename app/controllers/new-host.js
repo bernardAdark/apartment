20562607854922c6e991fd2b13180093dac51cc6
Yaw Boakye <wheresyaw@gmail>
2015-11-22 14:29:47 +0000

    Do not accept spouse details for single hosts

--- app/controllers/new-host.js
+++ app/controllers/new-host.js
@@ -12,21 +12,32 @@ export default Ember.Controller.extend({
     },
 
     createHost() {
-      let newHost = this.store.createRecord('host', {
+      let params = {
         title: this.get('title'),
         firstName: this.get('firstName'),
         lastName: this.get('lastName'),
         married: this.get('married'),
         occupation: this.get('occupation'),
-        spouseTitle: this.get('spouseTitle'),
-        spouseFirstName: this.get('spouseFirstName'),
-        spouseLastName: this.get('spouseLastName'),
         photo: this.get('photo'),
         phoneNumbers: this.get('phoneNumbers'),
         gender: this.get('gender'),
         religion: this.get('religion')
-      });
+      };
 
+      if (this.get('married') === 0) {
+        if (this.get('spouseTitle') || this.get('spouseFirstName') || this.get('spouseLastName')) {
+          console.error('Unmarried host cannot have spouse details');
+          this.transitionToRoute('new-host');
+        }
+      }
+
+      if (this.get('married') === 1) {
+        params['spouseTitle'] = this.get('spouseTitle');
+        params['spouseFirstName'] = this.get('spouseFirstName');
+        params['spouseLastName'] = this.get('spouseLastName');
+      }
+
+      let newHost = this.store.createRecord('host', params);
       newHost.save().
         then(this.transitionToRoute('homes')).
         catch(e => console.log(e.errors));
