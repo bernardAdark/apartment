abe3a137d7896bd3826197ced62a709f6b75684a
Precious N. E. Nyarko <precious.ewusi.nyarko@meltwater.org>
2015-11-22 00:05:30 +0000

    Remove pluralize helper
    Not serving any real purpose at the moment

--- app/helpers/pluralize-word.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export function pluralizeWord(params, namedArgs) {
-  var word = params[0];
-  return namedArgs.count === 0 || namedArgs.count > 1 ? Ember.String.pluralize(word) : word;
-}
-
-export default Ember.Helper.helper(pluralizeWord);
