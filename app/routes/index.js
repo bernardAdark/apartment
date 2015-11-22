59ebda37dea952694a685640ee0f4a1d977e2efd
Yaw Boakye <wheresyaw@gmail>
2015-11-22 14:26:22 +0000

    Do not fetch towns, suburbs on homepage
    In the near future we'll add a search box (instead of `<select>`
    elements) where the user can type name of city or suburb where they want
    an apartment.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,10 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-		return Ember.RSVP.hash({
-      towns: this.store.findAll('town'),
-      suburbs: this.store.findAll('suburb'),
-      homes: this.store.findAll('home')
-		});
+    return this.store.findAll('home');
   }
 });
