3c812341b99733ab53b75a3943616421fc6693b7
Yaw Boakye <wheresyaw@gmail>
2015-12-04 11:32:08 +0000

    Add Mapbox script and stylesheet via Bower

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,24 +6,12 @@ module.exports = function(defaults) {
     // Add options here
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-  
-  // Additional Libraries
   app.import('vendor/custom-bootstrap/js/bootstrap.js'); 
   app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
   app.import('vendor/custom-font-awesome/font-awesome.min.css');
   app.import('bower_components/animate.css/animate.min.css');
+  app.import('bower_components/mapbox.js/mapbox.js');
+  app.import('bower_components/mapbox.js/mapbox.css');
 
   return app.toTree();
 };
