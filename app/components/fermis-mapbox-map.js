68c32449d237d6c0c88185cd38c3e7dcf9435016
Yaw Boakye <wheresyaw@gmail>
2015-12-05 23:08:42 +0000

    Show suburbs listed on Apartments, with markers
    In other news I (re-)added the fermis-mapbox-map components template,
    after I had to jump through hoops to handle click events.

--- app/components/fermis-mapbox-map.js
+++ app/components/fermis-mapbox-map.js
@@ -3,20 +3,43 @@ import config from 'apartment/config/environment';
 
 export default Ember.Component.extend({
   classNames: ['map', 'mapbox-map'],
-  attributeBindings: ['id'],
-
-  id: 'map',
+  mapCenter: config.ACCRA_GEO_COORDS,
+  markers: [],
 
   didInsertElement() {
     this._super(...arguments);
-    
-    L.mapbox.accessToken = config.MAPBOX_ACCESS_TOKEN;
-    let map = L.mapbox.map('map', 'mapbox.streets');
 
-    if (Ember.isArray(this.get('town.geoCoords')) && this.get('town.geoCoords').length === 2) {
-      map.setView(this.get('town.geoCoords'), 15);
+    L.mapbox.accessToken = config.MAPBOX_ACCESS_TOKEN;
+
+    const map = L.mapbox.map('map', this.get('mapType') || config.MAP_DEFAULT_TYPE).
+      setView(this.get('mapCenter'), this.get('zoomLevel') || config.MAP_DEFAULT_ZOOM_LEVEL);
+
+    if (this.get('town')) {
+      this.set('mapCenter', this.get('town.geoCoords'));
+
+      this.get('town').get('suburbs').forEach((s) => {
+        this.get('markers').push({
+          'type': 'Feature',
+          'geometry': {
+            'type': 'Point',
+            'coordinates': s.get('geoCoords')
+          },
+          'properties': {
+            'title': s.get('name'),
+            'description': s.get('description'),
+            'marker-size': config.MAP_MARKER_SIZE,
+            'marker-color': config.MAP_MARKER_COLOR,
+            'marker-symbol': config.MAP_MARKER_SYMBOLS.TOWN
+          }
+        });
+      });
+
+      L.mapbox.featureLayer().setGeoJSON(this.get('markers')).addTo(map);
     }
+
+    map.panTo(this.get('mapCenter'));
   },
+
 
   willDestroyElement() {
   }
