ea452badddb5be104126df7b21ebb27c1d93e479
Yaw Boakye <wheresyaw@gmail.com>
2015-12-07 17:07:18 +0000

    Move `model` hook in the base suburb route

--- app/routes/suburb/index.js
+++ app/routes/suburb/index.js
@@ -1,11 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.query('suburb', {orderBy: 'slug', equalTo: this.paramsFor('suburb').slug}).
-      then((subs) => { return subs.get('firstObject') });
-  },
-
   afterModel(model) {
     return model.get('deputies');
   }
