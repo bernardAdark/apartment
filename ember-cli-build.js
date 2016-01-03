fb640563aa66f0c5c85720d838d05a292d70d211
Yaw Boakye <Yaw Boakye>
2016-01-03 18:57:51 +0000

    Use .env for holding application secrets
    Also use nouislider (for slider component), and imgix.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,17 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    dotEnv: {
+      clientAllowedKeys: [
+        'FIREBASE_URL',
+        'MAPBOX_ACCESS_TOKEN',
+        'AWS_ACCESS_KEY_ID',
+        'AWS_SECRET_ACCESS_KEY',
+        'AWS_S3_BUCKET_NAME',
+        'AWS_REGION',
+        'IMGIX_URL'
+      ]
+    }
   });
 
   app.import('vendor/custom-bootstrap/js/bootstrap.js'); 
@@ -12,6 +22,9 @@ module.exports = function(defaults) {
   app.import('bower_components/animate.css/animate.min.css');
   app.import('bower_components/mapbox.js/mapbox.js');
   app.import('bower_components/mapbox.js/mapbox.css');
+  app.import('bower_components/nouislider/distribute/nouislider.min.js');
+  app.import('bower_components/nouislider/distribute/nouislider.min.css');
+  app.import('vendor/aws-sdk/aws-sdk-2.2.26.min.js');
 
   return app.toTree();
 };
