827ac1000a021ce19307d025aa6bc803571e092f
Yaw Boakye <Yaw Boakye>
2016-01-03 19:00:38 +0000

    Search WIP

--- app/controllers/town/index.js
+++ app/controllers/town/index.js
@@ -5,6 +5,18 @@ export default Ember.Controller.extend({
     goToSuburb(suburbId) {
       let suburb = this.store.peekRecord('suburb', suburbId);
       return this.transitionToRoute('suburb', suburb);
+    },
+
+    search(features) {
+      Object.keys(features).forEach((f) => {
+        !features[f] && (delete features[f])
+      });
+
+      console.log('Got here...');
+      this.set('results', this.store.peekAll('home'));
+      // return this.store.filter('home', (home) => {
+      //   home.get()
+      // });
     }
   }
 });
