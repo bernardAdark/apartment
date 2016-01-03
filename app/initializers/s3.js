b450544c9710f582032b0b78326e5d57a5f8f4e4
Yaw Boakye <Yaw Boakye>
2016-01-03 14:46:03 +0000

    Add an S3 initializer
    Inject into components so that it can be used by the file uploader component.

---
+++ app/initializers/s3.js
@@ -0,0 +1,27 @@
+import ENV from 'apartment/config/environment';
+
+export function initialize(application) {
+  let S3 = Ember.Object.extend({
+    init() {
+      AWS.config.update({
+        accessKeyId: ENV.AWS_ACCESS_KEY_ID,
+        secretAccessKey: ENV.AWS_SECRET_ACCESS_KEY,
+        region: ENV.AWS_REGION
+      });
+
+      AWS.config.sslEnabled = true;
+      AWS.config.logger = console;
+
+      let __bckt = new AWS.S3({params: {Bucket: ENV.AWS_S3_BUCKET_NAME}});
+      this.set('bucket', __bckt);
+    },
+  });
+
+  application.register('s3:main', S3);
+  application.inject('component', 's3', 's3:main');
+}
+
+export default {
+  name: 's3',
+  initialize
+};
