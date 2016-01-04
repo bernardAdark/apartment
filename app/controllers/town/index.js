6bfe83f2835b561a8a586deb010c203e9a50d54f
Yaw Boakye <wheresyaw@gmail.com>
2016-01-04 02:19:25 +0000

    :fire: Filter store records based on features
    We're still in the early days of implementing a lightning fast search.

--- app/controllers/town/index.js
+++ app/controllers/town/index.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  within(array, number) {
+    Ember.assert('Must have only 2 elements', array.length === 2);
+    return number >= array[0] && number <= array[1];
+  },
+
   actions: {
     goToSuburb(suburbId) {
       let suburb = this.store.peekRecord('suburb', suburbId);
@@ -8,15 +13,22 @@ export default Ember.Controller.extend({
     },
 
     search(features) {
+      let __amenities = [],
+          __other = 'price period couple furnished pets kitchen'.w();
+
       Object.keys(features).forEach((f) => {
-        !features[f] && (delete features[f])
+        if (!__other.contains(f)) {
+          (features[f] && __amenities.push(f)) || delete features[f];
+        }
       });
 
-      console.log('Got here...');
-      this.set('results', this.store.peekAll('home'));
-      // return this.store.filter('home', (home) => {
-      //   home.get()
-      // });
+      let self = this;
+      this.set('results', this.store.filter('home', function(home) {
+        let __amn = home.get('amenities');
+        return self.within(features['price'], home.get('price')) &&
+               self.within(features['period'], home.get('period')) && 
+               !__amenities.any((a) => { return !__amn[a]['available'] });
+      }));
     }
   }
 });
